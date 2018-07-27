c14129595a8b47591c67baffe51942defcac2ca6
Jouke van der Maas <j.maas@niposoftware.com>
2018-07-27 14:26:59 +0200

    bug fixes, color changes

--- seticon.js
+++ seticon.js
@@ -1,11 +1,33 @@
 (() => {
   const size = 16;
+  let oldFavIconHref = '';
+
+  const getLinkElement = () => {
+    let links = document.querySelectorAll('link#page-icon');
+
+    if (links.length) {
+      return links[0];
+    } else {
+      let link = document.createElement('link');
+      link.id = "page-icon";
+
+      document.head.appendChild(link);
+
+      return link
+    }
+  };
+
+  const originalLinkHref = getLinkElement().href;
 
   setInterval(() => {
   	let query = window.location.search.toLowerCase();
 
     if (query.indexOf('buildid') === -1) {
-    	// we're not on a build page
+      // we're not on a build page
+      let link = getLinkElement();
+
+      link.href = originalLinkHref;
+
       return;
     }
 
@@ -21,36 +43,30 @@
 
     let { color, fontFamily } = style;
 
-    // circle to make sure the icon is visible
-    ctx.fillStyle = '#fff';
-    ctx.beginPath();
-    ctx.arc(size/2, size/2 - 1, size/2, 0, Math.PI * 2, true);
-    ctx.fill();
-
-    // draw the icon
     ctx.font = `${size - 1.5}px ${fontFamily}`;
-    ctx.fillStyle = color;
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
 
+    let diameter = ctx.measureText(content).width;
+    let pos = (size - diameter) / 2;
+
+    // circle to make sure the icon is visible
+    ctx.fillStyle = '#ffffff';
+    ctx.beginPath();
+    ctx.arc(size/2, size/2 + 1, diameter / 2, 0, Math.PI * 2, true);
+    ctx.fill();
+
+    // draw the icon
+    ctx.fillStyle = color;
     ctx.fillText(content, size/2 - 0.5, size/2);
 
     // generate image
-    let existingLinks = document.querySelectorAll('link#page-icon');
-    let link;
-
-    if (existingLinks.length) {
-      link = existingLinks[0];
-    } else {
-      link = document.createElement('link');
-      link.id = "page-icon";
-    }
+    let link = getLinkElement();
 
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
 
     link.href = canvas.toDataURL("image/x-icon");
-    document.getElementsByTagName('head')[0].appendChild(link);
   }, 1000);
 
   console.log('Status favicon: running');

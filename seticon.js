2ce4f8c658f0c457307ae58846226e28aac9eb29
Jouke van der Maas <j.maas@niposoftware.com>
2018-10-17 14:44:07 +0200

    add release support

--- seticon.js
+++ seticon.js
@@ -2,6 +2,10 @@
   const size = 16;
   let oldFavIconHref = '';
 
+  const canvas = document.createElement('canvas');
+  canvas.width = size;
+  canvas.height = size;
+
   const getLinkElement = () => {
     let links = document.querySelectorAll('link#page-icon');
 
@@ -17,26 +21,11 @@
     }
   };
 
-  const originalLinkHref = getLinkElement().href;
-
-  setInterval(() => {
-  	let query = window.location.search.toLowerCase();
-
-    if (query.indexOf('buildid') === -1) {
-      // we're not on a build page
-      let link = getLinkElement();
-
-      link.href = originalLinkHref;
-
-      return;
-    }
-
-    const canvas = document.createElement('canvas');
-    canvas.width = size;
-    canvas.height = size;
-
+  const createForBuildPage = () => {
     const ctx = canvas.getContext('2d');
     ctx.imageSmoothingEnabled = true;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     let icon = document.querySelector('.result-status i[role="presentation"]');
     let content = icon.innerText;
     let style = getComputedStyle(icon);
@@ -48,7 +37,6 @@
     ctx.textBaseline = 'middle';
 
     let diameter = ctx.measureText(content).width;
-    let pos = (size - diameter) / 2;
 
     // circle to make sure the icon is visible
     ctx.fillStyle = '#ffffff';
@@ -59,6 +47,66 @@
     // draw the icon
     ctx.fillStyle = color;
     ctx.fillText(content, size/2 - 0.5, size/2);
+
+    return true;
+  };
+
+  const createForReleasePage = () => {
+    const ctx = canvas.getContext('2d');
+    ctx.imageSmoothingEnabled = true;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let icon = document.querySelector('.cd-environment-status-badge i[role="presentation"]');
+
+    if (icon === null) return false;
+
+    let content = getComputedStyle(icon, ':before').content.replace(/"/g, '');
+    let { color, fontFamily } = getComputedStyle(icon);
+    let { backgroundColor } = getComputedStyle(icon.parentElement);
+
+    ctx.font = `${size - 7}px ${fontFamily}`;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+
+    // circle to make sure the icon is visible
+    ctx.fillStyle = '#ffffff';
+    ctx.beginPath();
+    ctx.arc(size/2, size/2 + 1, (size - 1) / 2, 0, Math.PI * 2, true);
+    ctx.fill();
+
+    // draw the icon
+    ctx.fillStyle = backgroundColor;
+    ctx.beginPath();
+    ctx.arc(size/2, size/2, (size - 1) / 2, 0, Math.PI * 2, true);
+    ctx.fill();
+
+    ctx.fillStyle = color;
+    ctx.fillText(content, size/2 + .5, size/2);
+
+    return true;
+  };
+
+  const originalLinkHref = getLinkElement().href;
+
+  setInterval(() => {
+  	let query = window.location.search.toLowerCase();
+
+    var success = false;
+
+    if (query.indexOf('buildid') !== -1) {
+      success = createForBuildPage();
+    } else if (query.indexOf('releaseid') !== -1) {
+      success = createForReleasePage();
+    }
+
+    if (!success) {
+      // we're not on a build page
+      let link = getLinkElement();
+
+      link.href = originalLinkHref;
+
+      return;
+    }
 
     // generate image
     let link = getLinkElement();
@@ -71,3 +119,4 @@
 
   console.log('Status favicon: running');
 })()
+
